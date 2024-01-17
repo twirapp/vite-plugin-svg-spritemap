b9363bd830e57b6b3f90392610dd7789a010ca85
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-01-17 16:18:26 +0800

    chore: update build script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,6 @@
-import { promises as fs } from 'node:fs'
+import fs from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { defineConfig } from 'tsup'
-import fg from 'fast-glob'
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -9,16 +8,9 @@ export default defineConfig({
   shims: true,
   dts: true,
   async onSuccess() {
-    // Add styles templates for css generation
-    const styles = async () => {
-      const files = await fg('src/styles/*.(scss|styl|less)')
-      for (const file of files)
-        await fs.copyFile(file, file.replace('src/styles', 'dist/'))
-    }
-
-    Promise.all([
-      fs.copyFile(resolve(__dirname, './src/client.d.ts'), resolve(__dirname, 'dist/client.d.ts')),
-      styles(),
-    ])
+    await fs.copyFile(
+      resolve(__dirname, './src/client.d.ts'),
+      resolve(__dirname, 'dist/client.d.ts')
+    )
   },
 })
